fce1f030f1927a9f3e344488bae6f1aaf5bd11d5
Dezmon Fernandez <defernan@mines.edu>
2015-03-24 19:45:04 -0600

    initial start only memset.c incorporated, need to fix to pass tests

--- src/memset.c
+++ src/memset.c
@@ -5,6 +5,13 @@
 void *memset(void *s, int c, size_t n)
 {
    // TODO: IMPLEMENT THIS FUNCTION
-
-   return NULL;
+   /* 
+    unsigned char* ptr = s;
+    int i; 
+    for(i = n; i > 0; i--){
+        unsigned char temp = (unsigned char)c;
+        *ptr++ = temp;
+    }
+    return s;*/
+    return NULL;
 }
