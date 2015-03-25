fce11f6861ad4c4d67f3e0513d978d113ecf159f
Dezmon Fernandez <defernan@mines.edu>
2015-03-24 20:31:48 -0600

    intermediate 1 tests successful

--- src/memset.c
+++ src/memset.c
@@ -5,13 +5,13 @@
 void *memset(void *s, int c, size_t n)
 {
    // TODO: IMPLEMENT THIS FUNCTION
-   /* 
     unsigned char* ptr = s;
     int i; 
-    for(i = n; i > 0; i--){
+    for(i = 0; i < n; i++){
         unsigned char temp = (unsigned char)c;
-        *ptr++ = temp;
+        *ptr = temp;
+        ptr++;
+         
     }
-    return s;*/
-    return NULL;
+    return s; 
 }
